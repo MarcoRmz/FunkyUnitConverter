81cbc6df9134ff48d14b2212b2583925b016c8a7
MarcoRmz <marcormz19@gmail.com>
2015-01-06 14:03:59 -0600

    initial commit

---
+++ FunkyUnitConverter/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  FunkyUnitConverter
+//
+//  Created by Marco Ramirez on 1/6/15.
+//  Copyright (c) 2015 Marco Ramirez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //X variable type integer
+    int x;
+    //X is assigned value 5
+    x = 5;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
